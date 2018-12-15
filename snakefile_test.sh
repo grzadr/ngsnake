1ba5be3c3351998d38d0e33e515ec4772ec2714f
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-15 18:47:00 +0100

    gitingore & fixed snakemake_test.sh

--- snakefile_test.sh
+++ snakefile_test.sh
@@ -3,8 +3,8 @@
 docker pull grzadr/biosak:mapping
 docker run -it \
   -v /etc/localtime:/etc/localtime:ro \
-  -v /data/OPUS:/data \
-  -v ~/Git/Public/ngsake:/home/jovyan/map \
+  -v ~/DSD:/data \
+  -v ~/Git/Public/ngsnake/:/home/jovyan/map/ \
   -w /home/jovyan/map \
   -it \
   --rm \
