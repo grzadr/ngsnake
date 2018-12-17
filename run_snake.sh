7803590e8a2b6960bede3503675b8c92246b9992
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-17 13:06:47 +0100

    Added argument for config file

--- run_snake.sh
+++ run_snake.sh
@@ -7,4 +7,4 @@ docker run -it \
   -v ${PWD}:/home/jovyan/map \
   -w /home/jovyan/map \
   --name ngsnake \
-  grzadr/biosak:mapping snakemake -pr -j ${2} ${3}
+  grzadr/biosak:mapping snakemake -pr -j ${2} --configfile ${3} ${4}
