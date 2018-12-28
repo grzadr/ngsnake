8531eb27083ed9451d15dfc5f38c44181a02b3d6
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-28 09:08:01 +0100

    Fixed issue with multiple arguemnts passed to run_snake function

--- run_snake.sh
+++ run_snake.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -eux
+
+# echo "${@:3}"
+# exit 0
 docker pull grzadr/biosak:mapping
 docker run -it \
   -v /etc/localtime:/etc/localtime:ro \
   -v ${1}:/data \
-  -v ${PWD}/Snakefile:/home/jovyan/map/ \
+  -v ${PWD}:/home/jovyan/map/ \
   -w /home/jovyan/map \
   --name ngsnake \
-  grzadr/biosak:mapping snakemake --configfile /data/config.yaml -pr -j ${2} ${3}
+  grzadr/biosak:mapping snakemake --configfile /data/config.yaml -pr -j ${2} "${@:3}"
--- test_snake.sh
+++ test_snake.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cp ${PWD}/Snakefile ${1}/Snakefile
+#cp ${PWD}/Snakefile ${1}/Snakefile
 docker pull grzadr/biosak:mapping
 docker run -it \
   -v /etc/localtime:/etc/localtime:ro \
   -v ${1}:/data \
-  -v ${PWD}/Snakefile:/home/jovyan/Snakefile \
+  -v ${PWD}:/home/jovyan/map \
+  -w /home/jovyan/map \
   -it \
   --rm \
   grzadr/biosak:mapping /bin/bash
