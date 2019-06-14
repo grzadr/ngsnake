995d0b86b040fcf8ec63a11fa764497944f06e1e
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-06-14 14:56:12 +0200

    Fixed docker image name

--- analyse.sh
+++ analyse.sh
@@ -4,6 +4,6 @@ DATADIR="/data/OPUS-2017-Obesity"
 
 ./run_snake.sh OPUS-Obesity-2017-SmallVariantCalling "$DATADIR" /ngsnake/Snakefile.small_variants /ngsnake/config.yaml multiqc_mapping_report
 ./run_snake.sh OPUS-Obesity-2017-SmallVariantCalling "$DATADIR" /ngsnake/Snakefile.small_variants /ngsnake/config.yaml call_variants
-./run_snake.sh OPUS-Obesity-2017-SmallVariantCalling "$DATADIR" /ngsnake/Snakefile.copy_number_variants /ngsnake/config.yaml call_cnvs
-./run_snake.sh OPUS-Obesity-2017-SmallVariantCalling "$DATADIR" /ngsnake/Snakefile.copy_number_variants /ngsnake/config.yaml call_cnmops_cvsc
+./run_snake.sh OPUS-2017-Obesity-CNVsCalling "$DATADIR" /ngsnake/Snakefile.copy_number_variants /ngsnake/config.yaml call_cnvs
+./run_snake.sh OPUS-2017-Obesity-CNVsCalling "$DATADIR" /ngsnake/Snakefile.copy_number_variants /ngsnake/config.yaml call_cnmops_cvsc
 
